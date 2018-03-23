0cf6e4d427564513d246034fa7ca183464d4c6b9
Enduror <teise-rudolf@hotmail.de>
2018-03-23 19:12:21 +0200

    Try if changes work
    I did a few things but it might be shit

---
+++ HookHiker/Assets/Scripts/Hook.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook : MonoBehaviour {
+    //-----------Timer------------// 
+	public float deathTimer;
+    public float timeCounter;
+   
+
+    //-----------Bullet------------// 
+    public float projectileVelocity;
+    public Vector2 shootingDirection;
+
+
+    //-----------PlayerControllerReference------------// 
+
+    public PlayerController playerController;  
+
+  
+    
+
+	
+	void Start () {
+        //setting counter to gamtime
+        timeCounter = Time.time;
+
+
+        // BulletDeathtimer and Velocity
+        deathTimer = 1;
+        projectileVelocity = 3;
+
+
+        //Referencing the playcontroller and giving a fixed shooting direction for every bullet
+        playerController = FindObjectOfType<PlayerController>();
+        shootingDirection = playerController.hookDirection;
+	}	
+
+	// Update is called once per frame
+	void Update ()
+    {       
+         travelToTarget(shootingDirection);
+        
+    }
+
+    //Method that kills Bullet after " Deathtimer" seconds
+	public void killBullet(){
+        Destroy(this.gameObject);
+	}   
+
+   //translate Funktion for bullet movement back and forth
+
+    public void travelToTarget(Vector2 target)
+    {
+        if (Time.time - timeCounter >= deathTimer)
+        {
+            target =new Vector2 (playerController.transform.position.x-transform.position.x,playerController.transform.position.y- transform.position.y);
+            projectileVelocity =3+target.magnitude/80;           
+          
+        }
+        transform.Translate(target * Time.deltaTime * projectileVelocity);
+        
+        
+    }
+    
+}
---
+++ HookHiker/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+//-----------------Character Attributes-------------------
+	public float speed;
+
+
+	
+    //---------------------------HookCooldown-------------------------
+    public float lastLeftHook;   // seperated Hook cd for left and Right.
+    public float lastRightHook; // seperated Hook cd for left and Right.
+    public float hookDelay;     // Time between left or right hook can be used again
+
+
+    //------------------------Vectors-----------------------------
+    public Vector2 hookDirection;    
+	public Vector3 mousePosition;
+	public Vector2 direction;
+
+
+	//I used a different Timer for left and right hook and initialize them here
+    //-------Problem---->>>  Cant shoot in the first secounds. bettersolution?? 
+	void Start ()
+    {
+        lastLeftHook = Time.time;
+        lastRightHook = Time.time;
+               
+    }
+
+	// Calculates the vector between player and mous. Controll check for Fire and Movement.
+	void Update () {
+		lookToMouse ();
+		horizontalCharacterMovement ();
+        fireHook();
+
+		}
+    // vector between played and mous normalized in screen to world point coordinates
+	public void lookToMouse () {
+		mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		direction = new Vector2 (mousePosition.x-transform.position.x, mousePosition.y-transform.position.y);
+		}
+
+    // Horizontal input and Movement.  
+    // Problem---->>>>> When character falls hes fucked. need translation in world horizontal instead of in relation to player transform.
+	public void horizontalCharacterMovement ()
+    {
+		float axisX = Input.GetAxis ("Horizontal");
+		transform.Translate (new Vector2 (axisX, 0) * Time.deltaTime * speed);
+    }
+
+
+        
+     // Method to fire a hook  in eather the left or the right side.
+    public void fireHook()
+    {        
+        // checks if hook is on cooldown and if left click was done left of the player
+        //instantiates a hook and resets the timer.
+        if (Time.time - lastLeftHook >= hookDelay &&(Input.GetMouseButtonDown(0) && mousePosition.x <= transform.position.x))
+        {
+                lastLeftHook = Time.time;
+                instantiateHook();                
+        }
+        // checks if hook is on cooldown and if right click was done left of the player
+        //instantiates a hook and resets the timer.
+        if ((Input.GetMouseButtonDown(1) && mousePosition.x >= transform.position.x)&& Time.time - lastRightHook >= hookDelay)
+        {
+                lastRightHook = Time.time;
+                instantiateHook();
+        }     
+    }
+
+    //instantiates a hook. hookDirection is taken by the Hook class 
+    // Hook prefab is Kinematic now due to the problem that the hook was initiated within the player and bouth have a hitbox.
+    public void instantiateHook()
+    {        
+        hookDirection = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        GameObject Hook = (GameObject)Instantiate(Resources.Load("Prefabs/Hook"), transform.position, Quaternion.identity);   
+    }
+    
+}
+
+
