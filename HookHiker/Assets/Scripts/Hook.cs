30c500031c1a5aab86fd05aaafc66ce7924fb422
walkerbrault <wbrault01@hamline.edu>
2018-03-23 02:31:40 +0200

    Hook spawning and deletion

---
+++ HookHiker/Assets/Scripts/Hook.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook : MonoBehaviour {
+	public float deathTimer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		killBullet ();
+	}
+
+	void killBullet(){
+		deathTimer += 1.0F * Time.deltaTime;
+		if (deathTimer >= 1) {
+			GameObject.Destroy(gameObject);
+		}
+	}
+}
---
+++ HookHiker/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+//-----------------Character Attributes-------------------
+	public float speed;
+
+
+	//--------------------------------------------------------
+
+	public Hook leftHook;
+	//private List<GameObject> Projectiles = new List<GameObject>();
+	public float projectileVelocity;
+	public Vector2 projectileDirection;
+	public Vector3 mousePosition;
+	public Vector2 direction;
+	public bool isReadyToShoot;
+	public bool isBulletAlive;
+
+
+
+	// Use this for initialization
+	void Start () {
+		speed = 20;
+		projectileVelocity = 2;
+		isReadyToShoot = true;
+		}
+
+	// Update is called once per frame
+	void Update () {
+		lookToMouse ();
+		horizontalCharacterMovement ();
+		fireLeftHook ();
+
+		}
+	public void lookToMouse () {
+		mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		direction = new Vector2 (mousePosition.x-transform.position.x, mousePosition.y-transform.position.y);
+		}
+
+	public void horizontalCharacterMovement (){
+		float axisX = Input.GetAxis ("Horizontal");
+		transform.Translate (new Vector2 (axisX, 0) * Time.deltaTime * speed);
+		}
+
+	public void fireLeftHook () {
+		if (Input.GetMouseButtonDown (0)) {
+			if (isReadyToShoot) {
+				GameObject lefty = (GameObject)Instantiate (Resources.Load("Prefabs/Hook"), transform.position, Quaternion.identity);
+				projectileDirection = direction;
+
+			}
+		}
+			//leftHook.transform.Translate (projectileDirection * Time.deltaTime * projectileVelocity);
+	}
+}
+
